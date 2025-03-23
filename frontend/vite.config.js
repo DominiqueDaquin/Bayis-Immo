bff105d6184a40427234e9380846099b5d3225f4
DominiqueDaquin <kouakepdominique@gmail.com>
2025-03-23 14:47:44 +0100

    build for development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,6 +10,19 @@ export default defineConfig({
     },
   },
   base: '/',
-
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  // server: {
+  //   historyApiFallback: true,
+  //   // Uniquement si vous avez besoin de proxy vers un backend
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
 });
-
