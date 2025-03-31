10310f808bcaeeddb5c7b0a178ca191da5e52d8e
DominiqueDaquin <kouakepdominique@gmail.com>
2025-03-31 13:21:00 +0200

    Modif config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 export const baseUrl="http://127.0.0.1:8000"   //"https://dakicodeur.pythonanywhere.com"
 
-export const baseUrlFrontend="http://localhost:5173"
+export const baseUrlFrontend= "https://bayisimmob.onrender.com" //"http://localhost:5173"
