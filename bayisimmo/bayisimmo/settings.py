cf615be8784ebf175d6954e4718e2137ae08bb79
DominiqueDaquin <kouakepdominique@gmail.com>
2025-04-08 13:31:06 +0200

    COnfiguration s3

--- bayisimmo/bayisimmo/settings.py
+++ bayisimmo/bayisimmo/settings.py
@@ -241,11 +241,20 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 if DEBUG==False:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # AWS_ACCESS_KEY_ID = 'votre-access-key'
-    # AWS_SECRET_ACCESS_KEY = 'votre-secret-key'
-    # AWS_STORAGE_BUCKET_NAME = 'nom-de-votre-bucket'
-    # AWS_S3_REGION_NAME = 'us-east-2'  # Région de votre bucket
-    # AWS_S3_FILE_OVERWRITE = False
-    # AWS_DEFAULT_ACL = None
-    # AWS_S3_VERIFY = True
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')  
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public'  
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_LOCATION = 'static'
+
+    # Stockage statique et media
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    # URLs pour les fichiers statiques et media
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
